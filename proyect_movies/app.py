e2854cb3a8d739c09252f3b2760cc10dadbd9d29
Puku <pikachuelpro123@gmail.com>
2026-09-11 06:46:51 -0500

    add app

--- proyect_movies/app.py
+++ proyect_movies/app.py
@@ -2,6 +2,7 @@ from models.movies import search_movie as search
 from models.similar_movies import similar_movies as similiar
 from models.queries import get_recommendations as recommended
 from flask import Flask, jsonify, request
+from models.user import register, login
 
 app = Flask(__name__) # The current file
 
@@ -11,8 +12,35 @@ def get_recommendations():
     result = recommended(id_user)
     return jsonify(result)
 
+@app.route("/register", methods=['POST'])
+def user_register():
+    data = request.get_json()
+
+    if data is None:
+        return jsonify({"Error": "no JSON received"}), 400
+
+    user_name = str(data.get('user_name'))
+    user_password = str(data.get('user_password'))
+
+    res = register(user_name, user_password)
+    return jsonify({'register': res})
+
+
+@app.route("/login", methods=['POST'])
+def user_login():
+    data = request.get_json()
+
+    if data is None:
+       return jsonify({"Error": "no JSON received"}), 400
+
+    user_name = str(data.get('user_name'))
+    user_password = str(data.get('user_password'))
+
+    res = login(user_name, user_password)
+    return str({'login',res})
+    
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
 
